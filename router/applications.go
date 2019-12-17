613c8beabead8366a34b6448508cea49992b2f9b
ぽ <magichiro-4869@i.softbank.jp>
2019-12-17 15:01:12 +0900

    #14 modelの削除にともないStringを返すだけに変更

--- router/applications.go
+++ router/applications.go
@@ -4,29 +4,16 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/traPtitech/Jomon/model"
 )
 
 func GetApplications(c echo.Context) error {
-	allapplications, err := model.GetApplications(c)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, allapplications)
-	}
-	return c.JSON(http.StatusOK, allapplications)
+	return c.String(http.StatusOK, "GetApplications")
 }
 
 func PostApplications(c echo.Context) error {
-	application, err := model.PostApplications(c)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, application)
-	}
-	return c.JSON(http.StatusOK, application)
+	return c.String(http.StatusOK, "PostApplications")
 }
 
 func PatchApplications(c echo.Context) error {
-	application, err := model.PatchApplications(c)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, application)
-	}
-	return c.JSON(http.StatusOK, application)
+	return c.String(http.StatusOK, "PatchApplications")
 }
