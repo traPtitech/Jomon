857205768134bfa2444c410ac9e382cbeb12eda6
sautzu <sautzu@yahoo.co.jp>
2020-01-05 03:18:59 +0900

    #10 一覧画面が出来た

---
+++ client/src/views/components/Application.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <v-row class="md5">
+      <v-col cols="4">
+        {{ list.title }}
+      </v-col>
+      <v-col cols="3">
+        {{ list.name }}
+      </v-col>
+      <v-col cols="2">
+        {{ list.money }}
+      </v-col>
+      <v-col cols="3">
+        {{ list.state }}
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Applicaton",
+  props: {
+    list: {
+      id: Number,
+      title: String,
+      name: String,
+      money: Number,
+      state: String
+    }
+  }
+};
+</script>
