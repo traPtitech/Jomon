4d7671a8eade3df62aa0dd2e1ff4ffb16be0b6d8
poispois <poispois00@gmail.com>
2020-03-02 10:27:53 +0900

    Implement RepayUser methods

--- model/repay_user.go
+++ model/repay_user.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"fmt"
+	"github.com/jinzhu/gorm"
 	"time"
 
 	"github.com/gofrs/uuid"
@@ -21,4 +23,25 @@ func (ru *RepayUser) GiveIsUserAdmin(admins []string) {
 
 	ru.RepaidToUserTrapID.GiveIsUserAdmin(admins)
 	ru.RepaidByUserTrapID.GiveIsUserAdmin(admins)
+}
+
+func (_ *applicationRepository) createRepayUser(db_ *gorm.DB, applicationId uuid.UUID, repaidToUserTrapID string) error {
+	ru := RepayUser{
+		ApplicationID: applicationId,
+		RepaidToUserTrapID: User{
+			TrapId: repaidToUserTrapID,
+		},
+	}
+
+	if !db_.Where(&ru).First(&RepayUser{}).RecordNotFound() {
+		return fmt.Errorf("already exists")
+	}
+
+	return db_.Create(&ru).Error
+}
+
+func (_ *applicationRepository) deleteRepayUserByApplicationID(db_ *gorm.DB, applicationId uuid.UUID) error {
+	return db_.Where(&RepayUser{
+		ApplicationID: applicationId,
+	}).Delete(&RepayUser{}).Error
 }
