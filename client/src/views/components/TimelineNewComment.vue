397dddf88677e331de5b7ece827083ddbd3ac0ab
series2 <series2.others@gmail.com>
2020-03-10 15:34:53 +0900

    コメントのポストと、フォーカスまわりのUI、バリデーション

--- client/src/views/components/TimelineNewComment.vue
+++ client/src/views/components/TimelineNewComment.vue
@@ -5,34 +5,59 @@
         <Icon :user="this.$store.state.me.trap_id" :size="25" />
       </v-col>
       <v-col cols="11">
-        <v-card ref="form" v-model="valid" :lazy-validation="lazy">
-          <v-textarea
-            v-model="name"
-            :counter="10"
-            :rules="nameRules"
-            solo
-            name="input-7-4"
-            label="Leave a comment"
-            required
-          ></v-textarea>
-          <v-btn
-            :disabled="!valid"
-            color="success"
-            class="mr-4"
-            @click="validate"
-          >
-            Comment
-          </v-btn>
+        <v-card class="pa-2">
+          <v-form ref="form" v-model="valid">
+            <v-textarea
+              @blur="blur"
+              v-model="comment"
+              :rules="nullRules"
+              outlined
+              label="Leave a comment"
+            ></v-textarea>
+            <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              @click="postcomment"
+            >
+              Comment
+            </v-btn>
+          </v-form>
         </v-card>
       </v-col>
     </v-row>
   </div>
 </template>
 <script>
+import axios from "axios";
 import Icon from "./Icon";
 export default {
+  data: () => {
+    return {
+      valid: true,
+      comment: "",
+      nullRules: [v => !!v || ""]
+    };
+  },
   components: {
     Icon
+  },
+  methods: {
+    blur() {
+      if (this.comment === "" || this.comment === undefined) {
+        this.$refs.form.reset();
+      }
+    },
+    postcomment() {
+      if (this.$refs.form.validate()) {
+        axios
+          .post("/api/applications/{applicationId}/comments", {
+            comment: this.comment
+          })
+          .then(response => console.log(response.status));
+        this.$refs.form.reset();
+      }
+    }
   }
 };
 </script>
