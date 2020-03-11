2e8c8486a1d663ec0916ff419488a92c6bded7f8
series2 <series2.others@gmail.com>
2020-03-11 15:35:57 +0900

    申請書ログの表記の改善（画面サイズによる処理分岐、文字サイズ）

--- client/src/views/components/Log.vue
+++ client/src/views/components/Log.vue
@@ -1,64 +1,82 @@
 <!-- ここはログの最小単位の表示を制御-->
 <template>
+  <!-- 以下はコメントログ -->
   <v-timeline-item v-if="log.log_type === `comment`" color="purple lighten-2">
     <template v-slot:icon>
-      <span>Com</span>
+      <Icon :user="log.content.user.trap_id" :size="25" />
     </template>
-    <v-card>
-      <v-card-title class="headline">
-        <v-row>
-          <v-col cols="8">
-            <Icon :user="log.content.user.trap_id" :size="25" />{{
-              log.content.user.trap_id
-            }}がコメントしました。</v-col
-          ><v-col cols="2">{{ dayPrint(log.content.created_at) }}</v-col
-          ><v-col cols="2">
-            <v-btn class="ma-2" tile outlined color="success">
-              <v-icon left>mdi-pencil</v-icon> Edit</v-btn
-            >
-          </v-col>
-        </v-row>
-      </v-card-title>
-      <v-card-text>
+    <v-card class="pa-2">
+      <v-row>
+        <v-col class="pb-0 pt-0" :class="grey_text" cols="8">
+          <strong :class="strong_text"> {{ log.content.user.trap_id }}</strong>
+          <span :class="larger_size">がコメントしました。</span
+          ><span :class="smaller_size">のコメント</span>
+        </v-col>
+        <v-col class="pa-0">{{ dayPrint(log.content.created_at) }}</v-col>
+        <v-col class="pa-0" cols="1">
+          <v-btn icon color="success">
+            <v-icon left>mdi-pencil</v-icon>
+          </v-btn>
+        </v-col>
+        <v-col class="pa-0" cols="1">
+          <v-btn icon color="error">
+            <v-icon>mdi-delete</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+
+      <v-divider></v-divider>
+      <v-card-text class="pa-0 black--text">
         {{ log.content.comment }}
       </v-card-text>
     </v-card>
   </v-timeline-item>
+  <!-- 以下は申請書の状態変化ログ -->
   <v-timeline-item
     v-else-if="log.log_type === `state`"
     color="red lighten-2"
     large
   >
-    <v-row justify="space-between">
-      <v-col cols="11">
-        <Icon :user="log.content.update_user.trap_id" :size="25" />{{
-          log.content.update_user.trap_id
-        }}が申請書の状態を{{ log.content.to_state }}に変更しました。</v-col
-      >
-      <v-col class="text-right" cols="1">{{
+    <v-row>
+      <v-col cols="10" :class="grey_text">
+        <Icon :user="log.content.update_user.trap_id" :size="25" /><strong
+          :class="strong_text"
+          >{{ log.content.update_user.trap_id }}</strong
+        >が
+        <span :class="larger_size"
+          >申請書の状態を<StateChip
+            :state="log.content.to_state"
+          />に変更しました。</span
+        >
+        <span :class="smaller_size"
+          ><StateChip :state="log.content.to_state" size="small" />に変更</span
+        >
+      </v-col>
+      <v-col class="text-right" cols="2">{{
         dayPrint(log.content.created_at)
       }}</v-col>
     </v-row>
     <v-card v-if="log.content.reason !== ''">
       <v-card-text>
         理由:
+        <span class="black--text">{{ log.content.reason }}</span>
       </v-card-text>
-      {{ log.content.reason }}
     </v-card>
   </v-timeline-item>
-
+  <!-- 以下は申請書の変更ログ -->
   <div v-else-if="log.log_type === `application`">
     <v-timeline-item color="purple lighten-2">
       <template v-slot:icon>
         <span>Application </span>
       </template>
       <v-row justify="space-between">
-        <v-col cols="11">
-          <Icon :user="log.content.update_user.trap_id" :size="25" />{{
-            log.content.update_user.trap_id
-          }}が修正しました。</v-col
+        <v-col cols="10" :class="grey_text">
+          <Icon :user="log.content.update_user.trap_id" :size="25" /><strong
+            :class="strong_text"
+            >{{ log.content.update_user.trap_id }}</strong
+          >が修正<span :class="larger_size">しました。</span></v-col
         >
-        <v-col class="text-right" cols="1">{{
+        <v-col class="text-right" cols="2">{{
           dayPrint(log.content.updated_at)
         }}</v-col>
       </v-row>
@@ -72,7 +90,7 @@
       {{ log.content.ammount }} paid_at{{ log.content.paid_at }}
     </v-card>
   </div>
-
+  <!-- 以下は払い戻しログ -->
   <v-timeline-item
     v-else-if="log.log_type === `repayment`"
     class="mb-4"
@@ -81,14 +99,18 @@
     small
   >
     <v-row justify="space-between">
-      <v-col cols="11">
-        <Icon :user="log.content.repaid_by_user.trap_id" :size="25" />{{
+      <v-col cols="10" :class="grey_text">
+        <Icon :user="log.content.repaid_by_user.trap_id" :size="25" />
+        <strong :class="strong_text">{{
           log.content.repaid_by_user.trap_id
-        }}が{{
-          log.content.repaid_to_user.trap_id
-        }}に払い戻しをしました。</v-col
-      >
-      <v-col class="text-right" cols="1">{{
+        }}</strong
+        >が
+        <strong :class="strong_text">{{
+          log.content.repaid_by_user.trap_id
+        }}</strong
+        >に 払い戻し<span :class="larger_size">をしました。</span>
+      </v-col>
+      <v-col class="text-right" cols="2">{{
         dayPrint(log.content.repaid_at)
       }}</v-col>
     </v-row>
@@ -97,12 +119,22 @@
 
 <script>
 import Icon from "./Icon";
+import StateChip from "./StateChip";
 export default {
+  data: function() {
+    return {
+      smaller_size: "hidden-lg-and-up",
+      larger_size: "hidden-md-and-down",
+      grey_text: "grey--text text--darken-1 body-2",
+      strong_text: "black--text subtitle-1"
+    };
+  },
   props: {
     log: Object
   },
   components: {
-    Icon
+    Icon,
+    StateChip
   },
   methods: {
     dayPrint(time) {
---
+++ client/src/views/components/StateChip.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-chip
+    :x-small="size === `x-small`"
+    :small="size === `small`"
+    :large="size === `large`"
+    :x-large="size === `x-large`"
+    :class="chip"
+    label
+    :style="style"
+    :width="width"
+  >
+    {{ this.state }}
+  </v-chip>
+</template>
+
+<script>
+export default {
+  name: "StateChip",
+  computed: {
+    chip: function() {
+      switch (this.state) {
+        case "submitted":
+          return "black white--text";
+        case "rejected":
+          return "yellow red--text";
+        case "fix_required":
+          return "orange white--text";
+        case "accepted":
+          return "red white--text";
+        case "fully_repaid":
+          return "gray black--text";
+        default:
+          return "white red--text";
+      }
+    }
+  },
+
+  props: {
+    state: {
+      type: String,
+      default: "fail"
+    },
+    size: {
+      type: String,
+      default: ""
+    },
+    style: {
+      type: String,
+      default: ""
+    },
+    width: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
