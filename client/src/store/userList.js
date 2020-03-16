7691849fdac59b796e906fe9d78f00d4e8ad0049
sautzu <sautzu@yahoo.co.jp>
2020-03-16 16:18:51 +0900

    getUserList

--- client/src/store/userList.js
+++ client/src/store/userList.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import Vue from "vue";
 export const userList = {
   state: [
     {
@@ -16,5 +18,23 @@ export const userList = {
       trap_id: "user4",
       is_admin: false
     }
-  ]
+  ],
+  mutations: {
+    setUserList(state, newState) {
+      state.splice(0);
+      newState.forEach((element, index) => {
+        Vue.set(state, index, element);
+      });
+    }
+  },
+  actions: {
+    async getUserList({ commit }) {
+      try {
+        const response = await axios.get("/api/users");
+        commit("setUserList", response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
 };
