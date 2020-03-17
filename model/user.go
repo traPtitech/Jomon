746721dec9a0f53737a4a75713457f259d535a17
ubuntu <modasokun@gmail.com>
2020-03-16 21:14:36 -0700

    GetUsers/GetMyUser/GetAdminUsersを実装

--- model/user.go
+++ model/user.go
@@ -1,5 +1,12 @@
 package model
 
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"net/http"
+)
+
 type User struct {
 	TrapId  string `gorm:"type:varchar(32);not null;" json:"trap_id"`
 	IsAdmin bool   `gorm:"-" json:"is_admin"`
@@ -18,4 +25,86 @@ func (user *User) GiveIsUserAdmin(admins []string) {
 			break
 		}
 	}
+}
+
+type traqUser struct {
+	Name string `json:"name"`
+}
+
+const baseURL = "https://q.trap.jp/api/1.0"
+
+func sendReqTraq(token string, req *http.Request) ([]byte, error) {
+	req.Header.Set("Authorization", token)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if res.StatusCode != 200 {
+		return nil, errors.New("認証に失敗しました")
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+func GetUsers(token string, admins []string, adminOnly bool) ([]User, error) {
+	req, err := http.NewRequest("GET", baseURL+"/users", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := sendReqTraq(token, req)
+	if err != nil {
+		return nil, err
+	}
+
+	traqUsers := []traqUser{}
+	if err = json.Unmarshal(body, &traqUsers); err != nil {
+		return nil, err
+	}
+
+	users := []User{}
+	for _, traqUser := range traqUsers {
+		user := User{
+			TrapId: traqUser.Name,
+		}
+
+		user.GiveIsUserAdmin(admins)
+		if adminOnly && !user.IsAdmin {
+			continue
+		}
+
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func GetMyUser(token string, admins []string) (User, error) {
+	req, err := http.NewRequest("GET", baseURL+"/users/me", nil)
+	if err != nil {
+		return User{}, err
+	}
+
+	body, err := sendReqTraq(token, req)
+	if err != nil {
+		return User{}, err
+	}
+
+	traqUser := traqUser{}
+	if err = json.Unmarshal(body, &traqUser); err != nil {
+		return User{}, err
+	}
+
+	user := User{
+		TrapId: traqUser.Name,
+	}
+	user.GiveIsUserAdmin(admins)
+
+	return user, nil
 }
--- router/users.go
+++ router/users.go
@@ -4,24 +4,52 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/Jomon/model"
 )
 
 func GetUsers(c echo.Context) error {
-	// some program
-	c.Response().Header().Set(echo.HeaderContentType, "application/json")
-	return c.String(http.StatusOK, "GetUsers")
+	token := c.Request().Header.Get("Authorization")
+	if token == "" {
+		return c.NoContent(http.StatusUnauthorized)
+	}
+
+	admins := []string{}
+	users, err := model.GetUsers(token, admins, false)
+	if err != nil {
+		return c.NoContent(http.StatusUnauthorized)
+	}
+
+	return c.JSON(http.StatusOK, users)
 }
 
 func GetMyUser(c echo.Context) error {
-	// some program
-	c.Response().Header().Set(echo.HeaderContentType, "application/json")
-	return c.String(http.StatusOK, "GetMyUser")
+	token := c.Request().Header.Get("Authorization")
+	if token == "" {
+		return c.NoContent(http.StatusUnauthorized)
+	}
+
+	admins := []string{}
+	user, err := model.GetMyUser(token, admins)
+	if err != nil {
+		return c.NoContent(http.StatusUnauthorized)
+	}
+
+	return c.JSON(http.StatusOK, user)
 }
 
 func GetAdminUsers(c echo.Context) error {
-	// some program
-	c.Response().Header().Set(echo.HeaderContentType, "application/json")
-	return c.String(http.StatusOK, "GetAdminUsers")
+	token := c.Request().Header.Get("Authorization")
+	if token == "" {
+		return c.NoContent(http.StatusUnauthorized)
+	}
+
+	admins := []string{}
+	users, err := model.GetUsers(token, admins, true)
+	if err != nil {
+		return c.NoContent(http.StatusUnauthorized)
+	}
+
+	return c.JSON(http.StatusOK, users)
 }
 
 func PutAdminUsers(c echo.Context) error {
