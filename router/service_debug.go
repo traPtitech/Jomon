bc3d3520114956248ae593386593945b5f02c2f9
poispois <poispois00@gmail.com>
2020-03-31 03:40:12 +0900

    Use build tag

---
+++ router/service_debug.go
@@ -0,0 +1,72 @@
+// +build debug
+
+package router
+
+import (
+	"fmt"
+	"github.com/traPtitech/Jomon/model"
+	storagePkg "github.com/traPtitech/Jomon/storage"
+	"os"
+)
+
+//noinspection GoDuplicate
+func NewService() Service {
+	fmt.Printf("\n !!! THIS IS DEBUG BUILD !!!\n\n")
+
+	dir := os.Getenv("UPLOAD_DIR")
+	if dir == "" {
+		dir = "./uploads"
+	}
+	local, err := storagePkg.NewLocalStorage(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	return Service{
+		Administrators: model.NewAdministratorRepository(),
+		Applications:   model.NewApplicationRepository(),
+		Comments:       model.NewCommentRepository(),
+		Images:         model.NewApplicationsImageRepository(&local),
+		Users: &debugUserRepository{
+			users: []model.User{
+				{
+					TrapId:  "MyUser",
+					IsAdmin: true,
+				},
+				{
+					TrapId:  "AdminUser",
+					IsAdmin: true,
+				},
+				{
+					TrapId:  "NormalUser1",
+					IsAdmin: false,
+				},
+				{
+					TrapId:  "NormalUser2",
+					IsAdmin: false,
+				},
+			},
+		},
+	}
+}
+
+type debugUserRepository struct {
+	users []model.User
+}
+
+func (d *debugUserRepository) GetUsers(token string) ([]model.User, error) {
+	return d.users, nil
+}
+
+func (d *debugUserRepository) GetMyUser(token string) (model.User, error) {
+	return d.users[0], nil
+}
+
+func (d *debugUserRepository) ExistsUser(token string, trapId string) (bool, error) {
+	for _, user := range d.users {
+		if trapId == user.TrapId {
+			return true, nil
+		}
+	}
+	return false, nil
+}
