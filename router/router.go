047a054175f3a6b1e678f60f327f6647b5fa2e7d
series2 <series2.others@gmail.com>
2020-04-29 14:01:42 +0900

    本番はgoからクライアント静的ファイルを配信するため、apiについてのルーティングを変更

--- router/router.go
+++ router/router.go
@@ -30,7 +30,9 @@ func SetRouting(e *echo.Echo, service Service) {
 		Root:  "client/dist",
 		HTML5: true,
 	}))
-	apiApplications := e.Group("/applications", service.AuthUserMiddleware)
+	api:=e.Group("/api")
+	{	
+		apiApplications :=api.Group("/applications", service.AuthUserMiddleware)
 	{
 		apiApplications.GET("", service.GetApplicationList)
 		apiApplications.POST("", service.PostApplication)
@@ -38,26 +40,26 @@ func SetRouting(e *echo.Echo, service Service) {
 		apiApplications.PATCH("/:applicationId", service.PatchApplication)
 	}
 
-	apiImages := e.Group("/images", service.AuthUserMiddleware)
+	apiImages :=api.Group("/images", service.AuthUserMiddleware)
 	{
 		apiImages.GET("/:imageId", service.GetImages)
 		apiImages.DELETE("/:imageId", service.DeleteImages)
 	}
 
-	apiComments := e.Group("/applications/:applicationId/comments", service.AuthUserMiddleware)
+	apiComments :=api.Group("/applications/:applicationId/comments", service.AuthUserMiddleware)
 	{
 		apiComments.POST("", service.PostComments)
 		apiComments.PUT("/:commentId", service.PutComments)
 		apiComments.DELETE("/:commentId", service.DeleteComments)
 	}
 
-	apiStatus := e.Group("/applications/:applicationId/states", service.AuthUserMiddleware)
+	apiStatus :=api.Group("/applications/:applicationId/states", service.AuthUserMiddleware)
 	{
 		apiStatus.PUT("", service.PutStates)
 		apiStatus.PUT("/repaid/:repaidToId", service.PutRepaidStates)
 	}
 
-	apiUsers := e.Group("/users", service.AuthUserMiddleware)
+	apiUsers :=api.Group("/users", service.AuthUserMiddleware)
 	{
 		apiUsers.GET("", service.GetUsers)
 		apiUsers.GET("/me", service.GetMyUser)
@@ -65,9 +67,11 @@ func SetRouting(e *echo.Echo, service Service) {
 		apiUsers.PUT("/admins", service.PutAdminUsers)
 	}
 
-	apiAuth := e.Group("/auth")
+	apiAuth :=api.Group("/auth")
 	{
 		apiAuth.GET("/callback", service.AuthCallback)
 		apiAuth.GET("/genpkce", service.GeneratePKCE)
 	}
+}
+
 }
