f20a2cfde01932cff355535495dd766c7d22e650
series2 <series2.others@gmail.com>
2020-05-22 01:10:14 +0900

    状態を表すチップの変更

--- client/src/views/shered/StateChip.vue
+++ client/src/views/shered/StateChip.vue
@@ -1,34 +1,52 @@
 <template>
-  <v-chip
-    :x-small="size === `x-small`"
-    :small="size === `small`"
-    :large="size === `large`"
-    :x-large="size === `x-large`"
-    :class="chip"
-    label
-  >
-    {{ this.state }}
-  </v-chip>
+  <span class="text-center">
+    <v-chip
+      :x-small="size === `x-small`"
+      :small="size === `small`"
+      :large="size === `large`"
+      :x-large="size === `x-large`"
+      :class="chip"
+      label
+    >
+      {{ state_text }}
+    </v-chip>
+  </span>
 </template>
 
 <script>
 export default {
   name: "StateChip",
   computed: {
-    chip: function() {
+    state_text() {
       switch (this.state) {
         case "submitted":
-          return "black white--text";
+          return "提出済";
         case "rejected":
-          return "yellow red--text";
+          return "却下";
         case "fix_required":
-          return "orange white--text";
+          return "要修正";
         case "accepted":
-          return "red white--text";
+          return "許可済";
         case "fully_repaid":
-          return "gray black--text";
+          return "払戻完";
         default:
-          return "white red--text";
+          return "error";
+      }
+    },
+    chip() {
+      switch (this.state) {
+        case "submitted":
+          return "yellow black--text";
+        case "rejected":
+          return "gray black--text";
+        case "fix_required":
+          return "red white--text";
+        case "accepted":
+          return "blue white--text";
+        case "fully_repaid":
+          return "green black--text";
+        default:
+          return "white black--text";
       }
     }
   },
