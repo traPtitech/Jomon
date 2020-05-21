c2179f5c1c9189e5df2e0bb405aa5e72be57bf85
series2 <series2.others@gmail.com>
2020-05-22 04:11:10 +0900

    色のテーマの導入

---
+++ client/src/plugins/theme.js
@@ -0,0 +1,10 @@
+export default {
+  primary: "#7B397b", //ヘッダーの色等
+  secondary: "#f0daee",
+  primary_accent: "#52254f",
+  secondary_accent: "#e0bfde",
+  error: "#f55a4e",
+  info: "#00d3ee",
+  success: "#5cb860",
+  warning: "#ffa21a"
+};
