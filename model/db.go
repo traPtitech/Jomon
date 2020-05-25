1c8565bcff8e0ef4e1104fb301c6c5bc3a9032ab
杉山祐紀 <magichiro-4869@i.softbank.jp>
2020-05-26 00:26:15 +0900

    Test用のEstablishConnection関数を作成(BlockGlobalUpdateがfalseなもの)

--- model/db.go
+++ model/db.go
@@ -47,6 +47,32 @@ func EstablishConnection() (*gorm.DB, error) {
 	return db, err
 }
 
+func EstablishConnectionForTest() (*gorm.DB, error) {
+	user := os.Getenv("MARIADB_USERNAME")
+	if user == "" {
+		user = "root"
+	}
+
+	pass := os.Getenv("MARIADB_PASSWORD")
+	if pass == "" {
+		pass = "password"
+	}
+
+	host := os.Getenv("MARIADB_HOSTNAME")
+	if host == "" {
+		host = "localhost"
+	}
+
+	dbname := os.Getenv("MARIADB_DATABASE")
+	if dbname == "" {
+		dbname = "jomon"
+	}
+
+	_db, err := gorm.Open("mysql", fmt.Sprintf("%s:%s@(%s)/%s", user, pass, host, dbname)+"?charset=utf8mb4&parseTime=True&loc=Local")
+	db = _db
+	return db, err
+}
+
 func Migrate() error {
 	if err := db.AutoMigrate(allTables...).Error; err != nil {
 		return err
--- model/db_test.go
+++ model/db_test.go
@@ -23,7 +23,7 @@ func TestMain(m *testing.M) {
 }
 
 func setupDB() *gorm.DB {
-	db, err := EstablishConnection()
+	db, err := EstablishConnectionForTest()
 	if err != nil {
 		panic(err)
 	}
