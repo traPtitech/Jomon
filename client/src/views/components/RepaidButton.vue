d79f79c04f09e26faa54b58cc5a835c32ffa8af3
sautzu <sautzu@yahoo.co.jp>
2020-05-30 19:41:32 +0900

    #102 支払日選択を追加

--- client/src/views/components/RepaidButton.vue
+++ client/src/views/components/RepaidButton.vue
@@ -10,13 +10,34 @@
     />
     払い戻し完了ボタン:
     <span v-for="user in repaidtotrapid" :key="user">
-      <v-btn color="primary" dark v-on:click="putRepaid(user)">{{
-        user
-      }}</v-btn>
+      <v-menu>
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" dark @click="repay.name = user" v-on="on">{{
+            user
+          }}</v-btn>
+        </template>
+        <v-date-picker
+          v-model="repay.date"
+          no-title
+          @input="dialog = true"
+        ></v-date-picker>
+      </v-menu>
     </span>
     <span v-if="repaidtotrapid.length === 0">
       何かがおかしいです。一度リロードしなおしてみて下さい。
     </span>
+    <v-dialog v-model="dialog">
+      <v-card>
+        <v-card-title>
+          <span>{{ repay.name }}へ払い戻ししますか？</span>
+        </v-card-title>
+        <br />
+        支払日:{{ repay.date }}
+        <br />
+        <v-btn color="primary" @click="dialog = false">no</v-btn
+        ><v-btn color="primary" @click="putRepaid(repay.name)">yes</v-btn>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 <script>
@@ -26,6 +47,13 @@ export default {
   components: {
     WithReasonButton
   },
+  data: () => ({
+    dialog: false,
+    repay: {
+      date: "",
+      name: ""
+    }
+  }),
   methods: {
     putRepaid(repaid_to_trap_id) {
       axios
@@ -34,13 +62,13 @@ export default {
             this.$store.state.application_detail_paper.core.application_id +
             "/states/repaid/" +
             repaid_to_trap_id,
-          { repaid_at: this.getNow() }
+          { repaid_at: this.getDate() }
         )
         .then(response => console.log(response.status));
       alert(repaid_to_trap_id + "に払い戻ししました。");
     },
-    getNow() {
-      let date = new Date();
+    getDate() {
+      let date = new Date(this.repay.date);
       let y = date.getFullYear();
       let m = ("00" + (date.getMonth() + 1)).slice(-2);
       let d = ("00" + date.getDate()).slice(-2);
