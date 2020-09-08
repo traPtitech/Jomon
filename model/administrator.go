a44aa7f2c84c2dc1ca05f5bfd1537ddb110bfb81
xxpoxx <magichiro-4869@i.softbank.jp>
2020-09-09 00:53:44 +0900

    Revert "Change AddAdministrator(Create to FirstOrCreate)"
    This reverts commit bc59bff66469226f8b7072efd0674cd8771c1e95.

--- model/administrator.go
+++ model/administrator.go
@@ -44,7 +44,7 @@ func (_ administratorRepository) GetAdministratorList() ([]string, error) {
 
 func (_ administratorRepository) AddAdministrator(userId string) error {
 	admin := Administrator{TrapID: userId}
-	return db.FirstOrCreate(&admin).Error
+	return db.Create(&admin).Error
 }
 
 func (_ administratorRepository) RemoveAdministrator(userId string) error {
