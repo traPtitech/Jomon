1a73fd575cd3e5b90e1945cc44f09cd6a03b932e
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2020-11-11 06:20:25 +0900

    use alias for src path

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
-import "./styles/index.scss";
+import App from "/@/App.vue";
+import router from "/@/router";
+import store from "/@/store";
+import vuetify from "/@/plugins/vuetify";
+import "/@/styles/index.scss";
 
 Vue.config.productionTip = false;
 
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,8 +1,18 @@
 const https = require("https");
-const keepAliveAgent = new https.Agent({ keepAlive: true });
+const path = require("path");
 const { DEV_SERVER_PROXY_HOST } = require("./dev.config");
 
+const keepAliveAgent = new https.Agent({ keepAlive: true });
+
 module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "/@": path.resolve(__dirname, "src").replace(/\\/g, "/")
+      },
+      extensions: [".js", ".vue", ".json"]
+    }
+  },
   transpileDependencies: ["vuetify"],
   devServer: {
     proxy: {
