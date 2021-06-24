3166df51e8df2e017fae11aeb5135eb7e4635f1f
xxpoxx <magichiro-4869@i.softbank.jp>
2021-06-24 22:00:30 +0900

    :construction: fix

--- model/model_test.go
+++ model/model_test.go
@@ -16,11 +16,11 @@ func SetupTestEntClient(t *testing.T) (*ent.Client, error) {
 	entOptions := []enttest.Option{
 		enttest.WithOptions(ent.Log(t.Log)),
 	}
-	dbUser := testutil.GetEnvOrDefault("MYSQL_USERNAME", "root")
-	dbPass := testutil.GetEnvOrDefault("MYSQL_PASSWORD", "password")
-	dbHost := testutil.GetEnvOrDefault("MYSQL_HOSTNAME", "db")
-	dbName := testutil.GetEnvOrDefault("MYSQL_DATABASE", "jomon")
-	dbPort := testutil.GetEnvOrDefault("MYSQL_PORT", "3306")
+	dbUser := testutil.GetEnvOrDefault("MARIADB_USERNAME", "root")
+	dbPass := testutil.GetEnvOrDefault("MARIADB_PASSWORD", "password")
+	dbHost := testutil.GetEnvOrDefault("MARIADB_HOSTNAME", "db")
+	dbName := testutil.GetEnvOrDefault("MARIADB_DATABASE", "jomon")
+	dbPort := testutil.GetEnvOrDefault("MARIADB_PORT", "3306")
 
 	dbDsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
 
