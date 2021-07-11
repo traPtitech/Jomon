ea4bf2c8970654c68edff67a60171dee56cc859b
shota-aa <dqx1005@icloud.com>
2021-07-11 19:39:03 +0900

    インデントと名前の修正をしました。

--- model/group.go
+++ model/group.go
@@ -11,7 +11,7 @@ type GroupRepository interface {
 	GetGroups(ctx context.Context) ([]*Group, error)
 	CreateGroup(ctx context.Context, name string, description string, budget *int, owners *[]User) (*Group, error)
 	GetMembers(ctx context.Context, groupID uuid.UUID) ([]*User, error)
-	CreateMember(ctx context.Context, groupID uuid.UUID, userID uuid.UUID) (*MemberID, error)
+	CreateMember(ctx context.Context, groupID uuid.UUID, userID uuid.UUID) (*Member, error)
 	DeleteMember(ctx context.Context, groupID uuid.UUID, userID uuid.UUID) error
 }
 
@@ -25,6 +25,6 @@ type Group struct {
 	DeletedAt   *time.Time
 }
 
-type MemberID struct {
+type Member struct {
 	ID uuid.UUID
 }
