a796aedeaf9425d42db7333f13d9f685d5e298f3
xxpoxx <magichiro-4869@i.softbank.jp>
2021-07-11 16:22:08 +0900

    :rotating_light: Impl file test

--- model/file_impl_test.go
+++ model/file_impl_test.go
@@ -1,13 +1,49 @@
 package model
 
-/*
-func TestCreateFile(t *testing.T) {
+import (
+	"context"
+	"fmt"
+	"mime"
+	"strings"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/traPtitech/Jomon/testutil/random"
+)
+
+func TestEntRepository_CreateFile(t *testing.T) {
 	client, storage, err := setup(t)
 	assert.NoError(t, err)
 	repo := NewEntRepository(client, storage)
 
 	t.Run("Success", func(t *testing.T) {
+		t.Parallel()
+		ctx := context.Background()
 
+		request, err := repo.CreateRequest(ctx, random.Numeric(t, 100000), random.AlphaNumeric(t, 20), random.AlphaNumeric(t, 50), nil, nil, nil)
+		assert.NoError(t, err)
+
+		sampleText := "sampleData"
+
+		fileID := uuid.New()
+		mimetype := "image/png"
+
+		ext, err := mime.ExtensionsByType(mimetype)
+		assert.NoError(t, err)
+		assert.True(t, len(ext) != 0)
+
+		filename := fmt.Sprintf("%s%s", fileID.String(), ext[0])
+
+		src := strings.NewReader(sampleText)
+
+		storage.EXPECT().
+			Save(filename, src).
+			Return(nil)
+
+		file, err := repo.CreateFile(ctx, src, random.AlphaNumeric(t, 20), mimetype, request.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, filename, file.Name)
+		assert.Equal(t, mimetype, file.MimeType)
 	})
 }
-*/
--- model/repository_test.go
+++ model/repository_test.go
@@ -1,20 +1,23 @@
 package model
 
 import (
-	"os"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/traPtitech/Jomon/ent"
-	"github.com/traPtitech/Jomon/storage"
+	"github.com/traPtitech/Jomon/storage/mock_storage"
 )
 
-func setup(t *testing.T) (*ent.Client, storage.Storage, error) {
+func setup(t *testing.T) (*ent.Client, *mock_storage.MockStorage, error) {
 	client, err := SetupTestEntClient(t)
 	if err != nil {
 		return nil, nil, err
 	}
-	os.Mkdir(os.Getenv("UPLOAD_DIR"), 0777)
-	storage, err := storage.NewLocalStorage(os.Getenv("UPLOAD_DIR"))
+	/*
+		os.Mkdir(os.Getenv("UPLOAD_DIR"), 0777)
+	*/
+	ctrl := gomock.NewController(t)
+	storage := mock_storage.NewMockStorage(ctrl)
 	if err != nil {
 		return nil, nil, err
 	}
