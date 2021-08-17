88904d6bdef9913b2c6a2f42e7f57aef47beacdf
xxpoxx <magichiro-4869@i.softbank.jp>
2021-08-17 21:54:25 +0900

    :rotating_light: add model file test

--- model/file_impl_test.go
+++ model/file_impl_test.go
@@ -2,6 +2,8 @@ package model
 
 import (
 	"context"
+	"fmt"
+	"mime"
 	"strings"
 	"testing"
 
@@ -37,5 +39,81 @@ func TestEntRepository_CreateFile(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Equal(t, name, file.Name)
 		assert.Equal(t, mimetype, file.MimeType)
+	})
+}
+
+func TestEntRepository_GetFile(t *testing.T) {
+	client, storage, err := setup(t)
+	assert.NoError(t, err)
+	repo := NewEntRepository(client, storage)
+
+	t.Run("Success", func(t *testing.T) {
+		t.Parallel()
+		ctx := context.Background()
+
+		var tags []*Tag
+		var group *Group
+		user, err := repo.CreateUser(ctx, random.AlphaNumeric(t, 20), random.AlphaNumeric(t, 20), false)
+		assert.NoError(t, err)
+		request, err := repo.CreateRequest(ctx, random.Numeric(t, 100000), random.AlphaNumeric(t, 20), random.AlphaNumeric(t, 50), tags, group, user.ID)
+		assert.NoError(t, err)
+
+		sampleText := "sampleData"
+
+		mimetype := "image/png"
+
+		src := strings.NewReader(sampleText)
+
+		name := random.AlphaNumeric(t, 20)
+
+		file, err := repo.CreateFile(ctx, src, name, mimetype, request.ID)
+		assert.NoError(t, err)
+		got, err := repo.GetFile(ctx, file.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, file.ID, got.ID)
+		assert.Equal(t, file.Name, got.Name)
+		assert.Equal(t, file.MimeType, got.MimeType)
+	})
+}
+
+func TestEntRepository_DeleteFile(t *testing.T) {
+	client, storage, err := setup(t)
+	assert.NoError(t, err)
+	repo := NewEntRepository(client, storage)
+
+	t.Run("Success", func(t *testing.T) {
+		t.Parallel()
+		ctx := context.Background()
+
+		var tags []*Tag
+		var group *Group
+		user, err := repo.CreateUser(ctx, random.AlphaNumeric(t, 20), random.AlphaNumeric(t, 20), false)
+		assert.NoError(t, err)
+		request, err := repo.CreateRequest(ctx, random.Numeric(t, 100000), random.AlphaNumeric(t, 20), random.AlphaNumeric(t, 50), tags, group, user.ID)
+		assert.NoError(t, err)
+
+		sampleText := "sampleData"
+
+		mimetype := "image/png"
+
+		src := strings.NewReader(sampleText)
+
+		name := random.AlphaNumeric(t, 20)
+
+		file, err := repo.CreateFile(ctx, src, name, mimetype, request.ID)
+		assert.NoError(t, err)
+
+		ext, err := mime.ExtensionsByType(file.MimeType)
+		assert.NoError(t, err)
+
+		storage.EXPECT().
+			Delete(fmt.Sprintf("%s%s", file.ID.String(), ext[0])).
+			Return(nil)
+
+		got, err := repo.DeleteFile(ctx, file.ID)
+		assert.NoError(t, err)
+		assert.Equal(t, file.ID, got.ID)
+		assert.Equal(t, file.Name, got.Name)
+		assert.Equal(t, file.MimeType, got.MimeType)
 	})
 }
