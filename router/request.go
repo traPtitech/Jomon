62385831f00edbb1f628addbf31a65721b78fe0b
shota-aa <dqx1005@icloud.com>
2022-02-20 00:11:29 +0900

    fix PutRequestStatus

--- router/request.go
+++ router/request.go
@@ -567,6 +567,7 @@ func (h *Handlers) PutStatus(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, err)
 	}
 
+	//TODO return Not Found
 	ctx := context.Background()
 	request, err := h.Repository.GetRequest(ctx, requestID)
 	if err != nil {
