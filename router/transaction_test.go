7fb9e23ec619e3727f330dd2032819bfd13c8040
H1rono <hronok66@gmail.com>
2024-07-20 00:37:13 +0900

    :adhesive_bandage: Remove unnecessary convert (unconvert)

--- router/transaction_test.go
+++ router/transaction_test.go
@@ -730,7 +730,7 @@ func TestHandlers_PostTransaction(t *testing.T) {
 
 		request := &model.RequestDetail{
 			ID:        uuid.New(),
-			Status:    model.Status(model.Accepted),
+			Status:    model.Accepted,
 			Title:     random.AlphaNumeric(t, 20),
 			Content:   random.AlphaNumeric(t, 50),
 			Comments:  []*model.Comment{},
