a95baa42f4e8bc5d6abc47044c5d233a367b1e2a
Hueter <kohsukemori@gmail.com>
2024-11-15 15:59:31 +0900

    change GetMe return model.User to router.User

--- router/user.go
+++ router/user.go
@@ -80,6 +80,18 @@ func (h Handlers) UpdateUserInfo(c echo.Context) error {
 	})
 }
 
+func userFromModelUser(u model.User) User {
+	return User{
+		ID:          u.ID,
+		Name:        u.Name,
+		DisplayName: u.DisplayName,
+		Admin:       u.Admin,
+		CreatedAt:   u.CreatedAt,
+		UpdatedAt:   u.UpdatedAt,
+		DeletedAt:   u.DeletedAt,
+	}
+}
+
 func (h Handlers) GetMe(c echo.Context) error {
 	sess, err := session.Get(h.SessionName, c)
 	if err != nil {
@@ -92,7 +104,7 @@ func (h Handlers) GetMe(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get user info")
 	}
 
-	user, err := h.Repository.GetUserByID(c.Request().Context(), userInSession.ID)
+	modelUser, err := h.Repository.GetUserByID(c.Request().Context(), userInSession.ID)
 	if err != nil {
 		if ent.IsNotFound(err) {
 			h.Logger.Error("failed to find user from DB by ID")
@@ -101,5 +113,6 @@ func (h Handlers) GetMe(c echo.Context) error {
 		h.Logger.Error("failed to get user by ID")
 		return c.JSON(http.StatusInternalServerError, err)
 	}
+	user := userFromModelUser(*modelUser)
 	return c.JSON(http.StatusOK, user)
 }
