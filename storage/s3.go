9aebbb79fee126ceae745d92afedf44ca990d60a
Hueter <kohsukemori@gmail.com>
2025-06-23 22:33:27 +0900

    fix s3.go from review

--- storage/s3.go
+++ storage/s3.go
@@ -7,35 +7,20 @@ import (
 	"io"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
-type S3FileStorage struct {
+type S3Storage struct {
 	bucket string
 	client *s3.Client
 }
 
-func NewS3FileStorage(
-	bucket string, region string, apiKey string, apiSecret string,
-) (*S3FileStorage, error) {
-	cfg, err := config.LoadDefaultConfig(context.Background(),
-		config.WithRegion(region),
-		config.WithCredentialsProvider(
-			credentials.NewStaticCredentialsProvider(apiKey, apiSecret, ""),
-		),
-	)
-
-	if err != nil {
-		return &S3FileStorage{}, err
-	}
-
+func NewS3Storage(cfg aws.Config, bucket string) (*S3Storage, error) {
 	client := s3.NewFromConfig(cfg)
 
-	s := &S3FileStorage{
+	s := &S3Storage{
 		bucket: bucket,
 		client: client,
 	}
@@ -43,26 +28,27 @@ func NewS3FileStorage(
 	return s, nil
 }
 
-func (fs *S3FileStorage) Save(filename string, key string, src io.Reader) error {
+func (fs *S3Storage) Save(ctx context.Context, filename string, key string, src io.Reader) error {
 	input := &s3.PutObjectInput{
-		Bucket:             aws.String(fs.bucket),
-		Key:                aws.String(key),
-		Body:               src,
+		Bucket: aws.String(fs.bucket),
+		Key:    aws.String(key),
+		Body:   src,
+		// FIXME : filenameに`"`が含まれているとバグりそう
 		ContentDisposition: aws.String(fmt.Sprintf("attachment; filename=\"%s\"", filename)),
 	}
 
 	uploader := manager.NewUploader(fs.client)
-	_, err := uploader.Upload(context.Background(), input)
+	_, err := uploader.Upload(ctx, input)
 
 	return err
 }
 
-func (fs *S3FileStorage) Open(key string) (io.ReadCloser, error) {
+func (fs *S3Storage) Open(ctx context.Context, key string) (io.ReadCloser, error) {
 	input := &s3.GetObjectInput{
 		Bucket: aws.String(fs.bucket),
 		Key:    aws.String(key),
 	}
-	file, err := fs.client.GetObject(context.Background(), input)
+	file, err := fs.client.GetObject(ctx, input)
 	if err != nil {
 		var nsk *types.NoSuchKey
 		if errors.As(err, &nsk) {
@@ -73,13 +59,13 @@ func (fs *S3FileStorage) Open(key string) (io.ReadCloser, error) {
 	return file.Body, nil
 }
 
-func (fs *S3FileStorage) Delete(key string) error {
+func (fs *S3Storage) Delete(ctx context.Context, key string) error {
 	input := &s3.DeleteObjectInput{
 		Bucket: aws.String(fs.bucket),
 		Key:    aws.String(key),
 	}
 
-	_, err := fs.client.DeleteObject(context.Background(), input)
+	_, err := fs.client.DeleteObject(ctx, input)
 	if err != nil {
 		var nsk *types.NoSuchKey
 		if errors.As(err, &nsk) {
