b472c9c18f55d49796c40ddb50630f83f9c0e474
Hueter <kohsukemori@gmail.com>
2025-06-23 21:39:36 +0900

    create storage s3.go

--- go.mod
+++ go.mod
@@ -22,6 +22,25 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/agext/levenshtein v1.2.3 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.36.4 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.10 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.29.16 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.69 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.31 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.17.79 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.35 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.35 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.35 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.12.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.7.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.12.16 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.18.16 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.80.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.25.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.30.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.33.21 // indirect
+	github.com/aws/smithy-go v1.22.2 // indirect
 	github.com/bmatcuk/doublestar v1.3.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-openapi/inflect v0.21.0 // indirect
---
+++ storage/s3.go
@@ -0,0 +1,91 @@
+package storage
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+
+	// "errors"
+	// "io"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+)
+
+
+type S3FileStorage struct {
+	bucket string
+	client *s3.Client
+}
+
+func NewS3FileStorage(bucket string, region string, apiKey string, apiSecret string) (*S3FileStorage, error) {
+	cfg, err := config.LoadDefaultConfig(context.Background(),
+		config.WithRegion(region),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(apiKey, apiSecret, "")),
+	)
+
+	if err != nil {
+		return &S3FileStorage{}, err
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	s := &S3FileStorage{
+		bucket: bucket,
+		client: client,
+	}
+
+	return s, nil
+}
+
+func (fs *S3FileStorage) Save(filename string, key string, src io.Reader) error {
+	input := &s3.PutObjectInput{
+		Bucket:             aws.String(fs.bucket),
+		Key:                aws.String(key),
+		Body:               src,
+		ContentDisposition: aws.String(fmt.Sprintf("attachment; filename=\"%s\"", filename)),
+	}
+
+	uploader := manager.NewUploader(fs.client)
+	_, err := uploader.Upload(context.Background(), input)
+
+	return err
+}
+
+func (fs *S3FileStorage) Open(key string) (io.ReadCloser, error) {
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(fs.bucket),
+		Key:    aws.String(key),
+	}
+	file, err := fs.client.GetObject(context.Background(), input)
+	if err != nil {
+		var nsk *types.NoSuchKey
+		if errors.As(err, &nsk) {
+			return nil, ErrFileNotFound
+		}
+		return nil, err
+	}
+	return file.Body, nil
+}
+
+func (fs *S3FileStorage) Delete(key string) error {
+	input := &s3.DeleteObjectInput{
+		Bucket: aws.String(fs.bucket),
+		Key:    aws.String(key),
+	}
+
+	_, err := fs.client.DeleteObject(context.Background(), input)
+	if err != nil {
+		var nsk *types.NoSuchKey
+		if errors.As(err, &nsk) {
+			return ErrFileNotFound
+		}
+		return err
+	}
+	return nil
+}
+
