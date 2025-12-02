036a67a0caf72f5e7563c3f97bcf837b9649b58a
rei <107461411+reiroop@users.noreply.github.com>
2025-12-03 01:19:38 +0900

    style: fix sass deprecation warnings by migrating to @use

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,7 @@
 import App from "@/App.vue";
 import vuetify from "@/plugins/vuetify";
 import router from "@/router";
+import "@/styles/index.scss";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 
