02c8c35bd3a9a478470749eb893ab0574bef551a
rei <107461411+reiroop@users.noreply.github.com>
2025-12-03 01:12:27 +0900

    chore: remove vuex and migrate test/config files to ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ["./tests/setup.js"],
+    setupFiles: ["./tests/setup.ts"],
     server: {
       deps: {
         inline: ["vuetify"]
