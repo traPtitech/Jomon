5cc04e40f26c068acdc5a60d20f8c2a7e29e2e36
rei <107461411+reiroop@users.noreply.github.com>
2025-12-04 04:05:57 +0900

    chore: update CI and dependencies
    - Add 'test' job to GitHub Actions workflow
    - Update prettier, eslint-plugin-prettier, @vue/eslint-config-prettier to latest versions

--- client/src/views/components/FixApplicationDetail.vue
+++ client/src/views/components/FixApplicationDetail.vue
@@ -310,7 +310,9 @@ onMounted(async () => {
   height: fit-content;
   margin: 12px;
   padding: 12px;
-  box-shadow: 0 3px 1px -2px rgb(0 0 0 / 20%), 0 2px 2px 0 rgb(0 0 0 / 14%),
+  box-shadow:
+    0 3px 1px -2px rgb(0 0 0 / 20%),
+    0 2px 2px 0 rgb(0 0 0 / 14%),
     0 1px 5px 0 rgb(0 0 0 / 12%);
 }
 .header {
