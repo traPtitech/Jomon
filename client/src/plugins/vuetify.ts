710152c0c8022ddcba715d0400f67b2e1a2a01f3
rei <107461411+reiroop@users.noreply.github.com>
2025-12-03 11:07:32 +0900

    perf: enable vuetify tree-shaking

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -1,13 +1,9 @@
 import "@mdi/font/css/materialdesignicons.css";
 import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
 // import "vuetify/styles"; // Removed as vite-plugin-vuetify handles this
 import light from "./theme";
 
 export default createVuetify({
-  components,
-  directives,
   theme: {
     defaultTheme: "light",
     themes: {
