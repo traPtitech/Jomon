3adc0a5fd066b171936ebbb5d433526b079bb140
H1rono <hronok66@gmail.com>
2026-02-23 22:08:16 +0900

    :adhesive_bandage: Fix `HTTPErrorHandlerInner`

--- internal/router/error.go
+++ internal/router/error.go
@@ -14,29 +14,33 @@ func HTTPErrorHandlerInner(err error) *echo.HTTPError {
 	if err == nil {
 		return nil
 	}
+	// FIXME(error-handling): type switchで最上位のエラーに対してhandlingする
 	if he := new(echo.HTTPError); errors.As(err, &he) {
 		return he
 	}
+	var herr error
 	if e := new(service.BadInputError); errors.As(err, &e) {
-		return echo.NewHTTPError(http.StatusBadRequest, e.Message).Wrap(e).(*echo.HTTPError)
+		herr = echo.NewHTTPError(http.StatusBadRequest, e.Message).Wrap(e)
+	} else if e := new(service.NotFoundError); errors.As(err, &e) {
+		herr = echo.NewHTTPError(http.StatusNotFound, e.Message).Wrap(e)
+	} else if e := new(service.ForbiddenError); errors.As(err, &e) {
+		herr = echo.NewHTTPError(http.StatusForbidden, e.Message).Wrap(e)
+	} else if e := new(service.UnauthenticatedError); errors.As(err, &e) {
+		herr = echo.NewHTTPError(http.StatusUnauthorized, e.Message).Wrap(e)
+	} else if e := new(service.UnexpectedError); errors.As(err, &e) {
+		herr = echo.ErrInternalServerError.Wrap(e)
+	} else if e := new(wrapsession.GetSessionError); errors.As(err, &e) {
+		herr = echo.ErrInternalServerError.Wrap(e)
+	} else if e := new(wrapsession.SaveSessionError); errors.As(err, &e) {
+		herr = echo.ErrInternalServerError.Wrap(e)
+	} else {
+		herr = echo.ErrInternalServerError.Wrap(err)
 	}
-	if e := new(service.NotFoundError); errors.As(err, &e) {
-		return echo.NewHTTPError(http.StatusNotFound, e.Message).Wrap(e).(*echo.HTTPError)
+	he := new(echo.HTTPError)
+	if !errors.As(herr, &he) {
+		// ここには来ないはず
+		code := http.StatusInternalServerError
+		return echo.NewHTTPError(code, http.StatusText(code))
 	}
-	if e := new(service.ForbiddenError); errors.As(err, &e) {
-		return echo.NewHTTPError(http.StatusForbidden, e.Message).Wrap(e).(*echo.HTTPError)
-	}
-	if e := new(service.UnauthenticatedError); errors.As(err, &e) {
-		return echo.NewHTTPError(http.StatusUnauthorized, e.Message).Wrap(e).(*echo.HTTPError)
-	}
-	if e := new(service.UnexpectedError); errors.As(err, &e) {
-		return echo.ErrInternalServerError.Wrap(e).(*echo.HTTPError)
-	}
-	if e := new(wrapsession.GetSessionError); errors.As(err, &e) {
-		return echo.ErrInternalServerError.Wrap(e).(*echo.HTTPError)
-	}
-	if e := new(wrapsession.SaveSessionError); errors.As(err, &e) {
-		return echo.ErrInternalServerError.Wrap(e).(*echo.HTTPError)
-	}
-	return echo.ErrInternalServerError.Wrap(err).(*echo.HTTPError)
+	return he
 }
