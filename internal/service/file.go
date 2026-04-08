5ec83984cd619a4076212c44d56a046b54e61b6c
H1rono <hronok66@gmail.com>
2026-04-08 17:13:53 +0900

    :sparkles: Add `Service` methods about file

--- internal/service/file.go
+++ internal/service/file.go
@@ -3,9 +3,12 @@ package service
 
 import (
 	"context"
+	"io"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/traPtitech/Jomon/internal/logging"
+	"go.uber.org/zap"
 )
 
 type FileRepository interface {
@@ -26,4 +29,37 @@ type File struct {
 	MimeType  string
 	CreatedBy uuid.UUID
 	CreatedAt time.Time
+}
+
+var acceptedMimeTypes = map[string]bool{
+	"image/jpeg":         true,
+	"image/png":          true,
+	"image/gif":          true,
+	"image/bmp":          true,
+	"application/pdf":    true,
+	"application/msword": true,
+	"application/zip":    true,
+}
+
+func (s *Service) WriteFile(
+	ctx context.Context,
+	userID, applicationID uuid.UUID,
+	name string, mimetype string, content io.Reader,
+) (*File, error) {
+	if !acceptedMimeTypes[mimetype] {
+		return nil, NewBadInputError("unsupported mime type")
+	}
+	logger := logging.GetLogger(ctx)
+	file, err := s.repository.CreateFile(ctx, name, mimetype, applicationID, userID)
+	if err != nil {
+		logger.Error("failed to create file in repository", zap.Error(err))
+		return nil, NewUnexpectedError(err)
+	}
+	err = s.storage.Save(ctx, file.ID.String(), content)
+	if err != nil {
+		logger.Error("failed to save file id in storage", zap.Error(err))
+		// TODO: storageが返すエラーはそのまま返したい
+		return nil, NewUnexpectedError(err)
+	}
+	return file, nil
 }
