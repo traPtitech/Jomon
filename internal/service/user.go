469edbba994a97fb690483b9959094676d13ec01
H1rono <hronok66@gmail.com>
2026-04-08 17:26:40 +0900

    :sparkles: Add `Service` methods about user

--- internal/service/user.go
+++ internal/service/user.go
@@ -6,7 +6,9 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/traPtitech/Jomon/internal/logging"
 	"github.com/traPtitech/Jomon/internal/nulltime"
+	"go.uber.org/zap"
 )
 
 type User struct {
@@ -27,4 +29,54 @@ type UserRepository interface {
 	UpdateUser(
 		ctx context.Context, userID uuid.UUID, name string, dn string, accountManager bool,
 	) (*User, error)
+}
+
+func (s *Service) GetUsers(ctx context.Context) ([]*User, error) {
+	logger := logging.GetLogger(ctx)
+	users, err := s.repository.GetUsers(ctx)
+	if err != nil {
+		logger.Error("failed to get users from repository", zap.Error(err))
+		return nil, err
+	}
+	return users, nil
+}
+
+func (s *Service) GetUserByID(ctx context.Context, userID uuid.UUID) (*User, error) {
+	logger := logging.GetLogger(ctx)
+	user, err := s.repository.GetUserByID(ctx, userID)
+	if err != nil {
+		logger.Error("failed to get user by id from repository", zap.Error(err))
+		return nil, err
+	}
+	return user, nil
+}
+
+func (s *Service) GetUserByName(ctx context.Context, name string) (*User, error) {
+	logger := logging.GetLogger(ctx)
+	user, err := s.repository.GetUserByName(ctx, name)
+	if err != nil {
+		logger.Error("failed to get user by name from repository", zap.Error(err))
+		return nil, err
+	}
+	return user, nil
+}
+
+type UpdateUserInputs struct {
+	Name           string
+	DisplayName    string
+	AccountManager bool
+}
+
+func (s *Service) UpdateUser(
+	ctx context.Context, userID uuid.UUID, inputs UpdateUserInputs,
+) (*User, error) {
+	logger := logging.GetLogger(ctx)
+	user, err := s.repository.UpdateUser(
+		ctx, userID, inputs.Name, inputs.DisplayName, inputs.AccountManager,
+	)
+	if err != nil {
+		logger.Error("failed to update user in repository", zap.Error(err))
+		return nil, err
+	}
+	return user, nil
 }
